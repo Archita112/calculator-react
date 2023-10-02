3556fccd95d34683aae5222e4ebfb8193ab408a9
Archita Sugandh <thecloudedsky00@gmail.com>
2023-10-02 20:27:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [display, setDisplay] = useState('0');
+  const [currentNumber, setCurrentNumber] = useState('');
+  const [previousNumber, setPreviousNumber] = useState('');
+  const [operator, setOperator] = useState('');
+
+  const handleNumberclick = (Number) => {
+    if (display === '0') {
+      setDisplay(Number);
+      setCurrentNumber(Number);
+    }
+    else{
+      setDisplay(display + Number);
+      setCurrentNumber(currentNumber + Number);
+    }
+  };
+
+  const handleOperatorclick = (selectedOperator) => {
+    if(previousNumber !== ''){
+      calculateResult();
+    }
+
+    setDisplay(selectedOperator);
+    setPreviousNumber(currentNumber);
+    setCurrentNumber('');
+    setOperator(selectedOperator);
+  };
+
+  const calculateResult = () => {
+    let result = 0;
+
+    switch (operator){
+      case '+':
+        result = parseFloat(previousNumber) + parseFloat(currentNumber);
+        break;
+
+      case '-':
+        result = parseFloat(previousNumber) - parseFloat(currentNumber);
+        break;
+        
+      case '*':
+        result = parseFloat(previousNumber) * parseFloat(currentNumber);
+        break;
+
+      case '/':
+        result = parseFloat(previousNumber) / parseFloat(currentNumber);
+        break;
+
+      default:
+        break;
+    }
+    setDisplay(result.toString());
+    setPreviousNumber('');
+    setCurrentNumber('');
+    setOperator('');
+  };
+
+  const handleClear = () => {
+    setDisplay('0');
+    setCurrentNumber('');
+    setPreviousNumber('');
+    setOperator('');
+  };
+
+  const handleEquals = () => {
+    calculateResult();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='upper-part'>{display}</div>
+      <div className='lower-part'>
+        <div className='section-1'>
+        <button onClick={handleClear}>C</button>
+        <button onClick={() => handleNumberclick('9')}>9</button>
+        <button onClick={handleEquals}>=</button>
+        <button onClick={() => handleOperatorclick('+')}>+</button>
+        </div>
+        <div className='section-2'>
+        <button onClick={() => handleNumberclick('8')}>8</button>
+        <button onClick={() => handleNumberclick('7')}>7</button>
+        <button onClick={() => handleNumberclick('6')}>6</button>
+        <button onClick={() => handleOperatorclick('-')}>-</button>
+        </div>
+        <div className='section-3'>
+        <button onClick={() => handleNumberclick('5')}>5</button>
+        <button onClick={() => handleNumberclick('4')}>4</button>
+        <button onClick={() => handleNumberclick('3')}>3</button>
+        <button onClick={() => handleOperatorclick('/')}>/</button>
+        </div>
+        <div className='section-4'>
+        <button onClick={() => handleNumberclick('2')}>2</button>
+        <button onClick={() => handleNumberclick('1')}>1</button>
+        <button onClick={() => handleNumberclick('0')}>0</button>
+        <button onClick={() => handleOperatorclick('*')}>*</button>
+        </div>
+      </div>
     </div>
   );
 }
